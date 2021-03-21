5b7a8ab9a258a6ed297309e9dfa5419e86519eea
mrgirzmet <samtebirah@gmail.com>
2021-03-21 17:15:55 +0300

    Initial Commit for this DEMO

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+
+type user = { 
+  userName: string,
+  userId: number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'trackByDemo';
+export class AppComponent implements OnInit {
+  users:user[] = [];
+  constructor(){}
+  ngOnInit(): void {
+    this.reset();
+  }
+
+  // Assigning Initial Data.
+  reset(){
+    this.users = [
+      { userName: "Ali", userId:1 },
+      { userName: "Tarik", userId:2 },
+      { userName: "Defne", userId:3 },
+      { userName: "Fatih", userId:4 },
+      { userName: "Erdem", userId:5 },
+    ]
+  }
+
+  // adding a user to current array
+  case1(){
+    this.users.push({userName:"pushUser",userId:6})
+  }
+
+  // assigning same array with new reference
+  case2(){
+    this.users = [
+      { userName: "Ali", userId:1 },
+      { userName: "Tarik", userId:2 },
+      { userName: "Defne", userId:3 },
+      { userName: "Fatih", userId:4 },
+      { userName: "Erdem", userId:5 },
+    ];
+  }
+
+  // assigning new Array with a additional value.
+  case3(){
+    this.users = [
+      { userName: "Ali", userId:1 },
+      { userName: "Tarik", userId:2 },
+      { userName: "Defne", userId:3 },
+      { userName: "Fatih", userId:4 },
+      { userName: "Erdem", userId:5 },
+      { userName: "Berk", userId:6 },
+    ];
+  }
+
+  // assigning new Array with shaked values.
+  case4(){
+    this.users = [
+      { userName: "Ali", userId:1 },
+      { userName: "Fatih", userId:4 },
+      { userName: "Erdem", userId:5 },
+      { userName: "Tarik", userId:2 },
+      { userName: "Defne", userId:3 },
+    ];
+  }
+
+  // assigning new Array with new value which is currently exist in older array. 
+  case5(){
+    this.users = [
+      { userName: "Ali", userId:1 },
+      { userName: "Tarik", userId:2 },
+      { userName: "Defne", userId:3 },
+      { userName: "Fatih", userId:4 },
+      { userName: "Erdem", userId:5 },
+      { userName: "Ali", userId:1 },
+    ];
+  }
+  
+  // pushing a value which is currently inside array.
+  case6(){
+    this.users.push({ userName: "Ali", userId:1 });
+  }
+
+  // changing values of users which same id.
+  case7(){
+    this.users = [
+      { userName: "Tarik", userId:1 },
+      { userName: "Ali", userId:2 },
+      { userName: "Defne", userId:3 },
+      { userName: "Fatih", userId:4 },
+      { userName: "Erdem", userId:5 },
+    ];
+  }
+
+  trackBy(index:number,user:user){
+    return user.userId;
+  }
+
+
 }
